99f19947184dd69bf7527140459e1d35ed2bc685
Clyde Caradine <clydejean.caradine@epita.fr>
2017-02-01 18:44:38 +0100

    [TC1][UNIQUE] change new with object_set_instance

--- lib/misc/unique.hxx
+++ lib/misc/unique.hxx
@@ -12,7 +12,7 @@ namespace misc
 {
   template <typename T, class C>
   unique<T, C>::unique(const data_type& s)
-    : obj_(&(*(new object_set_type()).insert().first))
+    : obj_(&(*object_set_instance().insert().first))
   {}
 
   template <typename T, class C>
