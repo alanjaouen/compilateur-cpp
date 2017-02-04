f54f8807724da29b730fa24f05be525c238a3d74
Raphaël Caruso <altiire@gmail.com>
2017-02-04 16:08:06 +0100

    [TC2][AST][FIELD-VAR] implemented FieldVar class' header

--- src/ast/field-var.hh
+++ src/ast/field-var.hh
@@ -11,11 +11,55 @@
 namespace ast
 {
 
-  /// FieldVar.
-  class FieldVar : public Var
-  {
-  // FIXME: Some code was deleted here.
-  };
+
+    /**
+     * \class ast::FieldVar
+     * \brief FIXME
+     *
+     * FIXME
+     */
+
+    /// FieldVar.
+    class FieldVar : public Var
+    {
+        public:
+            /**
+             * \name Ctor & dtor.
+             * \{ */
+            /// Construct a FieldVar node.
+            FieldVar(const Location& location, Var* var, Field* field);
+            /// Destroy a FieldVar node.
+            virtual ~FieldVar();
+            /** /} */
+
+            /**
+             * \name Visitors entry point.
+             * \{ */
+            /// Accept a const visitor \a v.
+            void accept(ConstVisitor& v) const override;
+            /// Accept a non-const visitor \a v.
+            void accept(Visitor& v) override;
+            /**  \} */
+
+            /**
+             * \name Accessors.
+             * \{ */
+            /// Return the field variable.
+            const Var& var_get() const;
+            /// Return the field variable.
+            Var& var_get();
+            /// Return the field of the variable.
+            const Field& field_get() const;
+            /// Return the field of the variable.
+            Field& field_get();
+            /** /} */
+
+        protected:
+            /// The field variable.
+            Var* var_;
+            /// The field of the variable.
+            Field* field_;
+    };
 
 } // namespace ast
 
