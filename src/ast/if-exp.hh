70ef625f88f79b592905226b93cbd736186d11ac
François Forestier <forest_b@epita.fr>
2017-02-04 16:45:58 +0100

    [TC2][IF-EXP] complete the If-exp.hh

--- src/ast/if-exp.hh
+++ src/ast/if-exp.hh
@@ -14,7 +14,42 @@ namespace ast
   /// IfExp.
   class IfExp : public Exp
   {
-  // FIXME: Some code was deleted here.
+        /** \name Ctor & dtor.
+     ** \{ */
+    /// Construct a IfExp node.
+    IfExp(Location& location, Exp* test, Exp* Bthen, Exp* Belse = nullptr);
+    /// Destroy a IfExp node.
+    virtual ~IfExp();
+    /** \} */
+
+    /// \name Visitors entry point.
+    /// \{ */
+    /// Accept a const visitor \a v.
+    void accept(ConstVisitor& v) const override;
+    /// Accept a non-const visitor \a v.
+    void accept(Visitor& v) override;
+    /// \}
+
+    /** \name Accessors.
+     ** \{ */
+    /// Return test declaration.
+    const Exp& test_get() const;
+    /// Return test declaration.
+    Exp& test_get();
+    /// Return instructions executed in the then.
+    const Exp& then_get() const;
+    /// Return instructions executed in the then..
+    Exp& then_get();
+    /// Return instructions executed in the else clause.
+    const Exp& else_get() const;
+    /// Return instructions executed in the else clause.
+    Exp& else_get();
+    /** \} */
+
+  protected:
+    Exp* test_;
+    Exp* Bthen_;
+    Exp* Belse_;
   };
 
 } // namespace ast
