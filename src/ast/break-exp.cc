6ca5ecab4e0318cb951ed642871936bc40c0008b
Clyde Caradine <clydejean.caradine@epita.fr>
2017-02-04 15:20:55 +0100

    [TC2][BREAK-EXP] fix .cc

--- src/ast/break-exp.cc
+++ src/ast/break-exp.cc
@@ -8,6 +8,23 @@
 
 namespace ast
 {
+  BreakExp::BreakExp(const Location& location) : Exp(location)
+  {}
+
+  BreakExp::~BreakExp()
+  {}
+
+  void
+  BreakExp::accept(ConstVisitor& v) const
+  {
+    v(*this);
+  }
+
+  void
+  BreakExp::accept(Visitor& v)
+  {
+    v(*this);
+  }
 
   // FIXME: Some code was deleted here.
 
