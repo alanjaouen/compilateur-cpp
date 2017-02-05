5167752e0947a516cebdc039916bcd621d2ded0f
Raphaël Caruso <altiire@gmail.com>
2017-02-05 12:02:29 +0100

    [TC2][AST][CALL-EXP] commented header

--- src/ast/call-exp.hh
+++ src/ast/call-exp.hh
@@ -13,47 +13,56 @@
 namespace ast
 {
 
-  /// CallExp.
-  class CallExp : public Exp
-  {
-  public:
-    /**
-     * \name Ctor & dtor.
-     * \{ */
-    /// Construct a CallExp node.
-    CallExp(const Location& location, misc::symbol* name, SeqExp::seq_type* seq);
-    /// Destroy a CallExp node.
-    virtual ~CallExp();
-    /** \} */
 
     /**
-     * \name Visitors entry point.
-     * \{ */
-    /// Accept a const visitor \a v.
-    void accept(ConstVisitor& v) const override;
-    /// Accept a non-const visitor \a v.
-    void accept(Visitor& v) override;
-    /** \} */
+     * \class ast::CallExp.
+     * \brief FIXME
+     *
+     * FIXME
+     */
 
-    /**
-     * \name Accessors.
-     * \{ */
-    /// Return the call expression.
-    const SeqExp::seq_type& seq_get() const;
-    /// Return the call expression.
-    SeqExp::seq_type& seq_get();
-    /// Return the function name.
-    const misc::symbol* name_get() const;
-    /// Return the function name.
-    misc::symbol* name_get();
-    /** \} */
+    /// CallExp.
+    class CallExp : public Exp
+    {
+        public:
+            /**
+             * \name Ctor & dtor.
+             * \{ */
+            /// Construct a CallExp node.
+            CallExp(const Location& location, misc::symbol* name,
+                    SeqExp::seq_type* seq);
+            /// Destroy a CallExp node.
+            virtual ~CallExp();
+            /** \} */
 
-  protected:
-    /// The call expression.
-    SeqExp::seq_type* seq_;
-    //The function name.
-    misc::symbol* name_;
-  };
+            /**
+             * \name Visitors entry point.
+             * \{ */
+            /// Accept a const visitor \a v.
+            void accept(ConstVisitor& v) const override;
+            /// Accept a non-const visitor \a v.
+            void accept(Visitor& v) override;
+            /** \} */
+
+            /**
+             * \name Accessors.
+             * \{ */
+            /// Return the call expression.
+            const SeqExp::seq_type& seq_get() const;
+            /// Return the call expression.
+            SeqExp::seq_type& seq_get();
+            /// Return the function name.
+            const misc::symbol* name_get() const;
+            /// Return the function name.
+            misc::symbol* name_get();
+            /** \} */
+
+        protected:
+            /// The call expression.
+            SeqExp::seq_type* seq_;
+            //The function name.
+            misc::symbol* name_;
+    };
 
 } // namespace ast
 
