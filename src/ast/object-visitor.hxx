5eafd481eb90ec131057080fc56a93a4e148f0b4
Raphaël Caruso <altiire@gmail.com>
2017-02-05 17:56:31 +0100

    [TC2][AST][OBJECT-VISITOR] implemented getter of GenObjectVisitor class

--- src/ast/object-visitor.hxx
+++ src/ast/object-visitor.hxx
@@ -12,54 +12,59 @@
 namespace ast
 {
 
-  template <template <typename> class Const>
-  GenObjectVisitor<Const>::GenObjectVisitor()
-    : GenVisitor<Const>()
-  {}
+    template <template <typename> class Const>
+        GenObjectVisitor<Const>::GenObjectVisitor()
+        : GenVisitor<Const>()
+        {}
 
-  template <template <typename> class Const>
-  GenObjectVisitor<Const>::~GenObjectVisitor()
-  {}
+    template <template <typename> class Const>
+        GenObjectVisitor<Const>::~GenObjectVisitor()
+        {}
 
 
-  /*-------------------------------.
-  | Object-related visit methods.  |
-  `-------------------------------*/
+    /*-------------------------------.
+      | Object-related visit methods.  |
+      `-------------------------------*/
 
-  template <template <typename> class Const>
-  void
-  GenObjectVisitor<Const>::operator()(const_t<ClassTy>& e)
-  {
-  // FIXME: Some code was deleted here.
-  }
+    template <template <typename> class Const>
+        void
+        GenObjectVisitor<Const>::operator()(const_t<ClassTy>& e)
+        {
+            e.super_get().accept(*this);
+            e.decs_get().accept(*this);
+        }
 
-  template <template <typename> class Const>
-  void
-  GenObjectVisitor<Const>::operator()(const_t<MethodDecs>& e)
-  {
-  // FIXME: Some code was deleted here.
-  }
+    template <template <typename> class Const>
+        void
+        GenObjectVisitor<Const>::operator()(const_t<MethodDecs>& e)
+        {
+            e.decs_get().accept(*this);
+        }
 
-  template <template <typename> class Const>
-  void
-  GenObjectVisitor<Const>::operator()(const_t<MethodDec>& e)
-  {
-  // FIXME: Some code was deleted here.
-  }
+    template <template <typename> class Const>
+        void
+        GenObjectVisitor<Const>::operator()(const_t<MethodDec>& e)
+        {
+            e.formals_get().accept(*this);
+            e.result_get().accept(*this);
+            e.body_get().accept(*this);
+        }
 
-  template <template <typename> class Const>
-  void
-  GenObjectVisitor<Const>::operator()(const_t<MethodCallExp>& e)
-  {
-  // FIXME: Some code was deleted here.
-  }
+    template <template <typename> class Const>
+        void
+        GenObjectVisitor<Const>::operator()(const_t<MethodCallExp>& e)
+        {
+            e.seq_get().accept(*this);
+            e.name_get().accept(*this);
+        }
 
-  template <template <typename> class Const>
-  void
-  GenObjectVisitor<Const>::operator()(const_t<ObjectExp>& e)
-  {
-  // FIXME: Some code was deleted here.
-  }
+    template <template <typename> class Const>
+        void
+        GenObjectVisitor<Const>::operator()(const_t<ObjectExp>& e)
+        {
+            e.exp_get().accept(*this);
+            e.type_get().accept(*this);
+        }
 
 
 } // namespace ast
