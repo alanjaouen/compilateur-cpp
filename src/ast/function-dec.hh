9de652a0ced42c4d679c05464d52b43f3c801326
Raphaël Caruso <altiire@gmail.com>
2017-02-05 15:43:00 +0100

    [TC2][AST][FUNCTION-DEC] commented header

--- src/ast/function-dec.hh
+++ src/ast/function-dec.hh
@@ -14,53 +14,63 @@
 namespace ast
 {
 
-  /// FunctionDec.
-  class FunctionDec : public Dec
-  {
-  public:
-    /** \name Ctor & dtor.
-     ** \{ */
-    /// Construct a FunctionDec node.
-    FunctionDec(const Location& location, const misc::symbol& name,
-                VarDecs* formals, NameTy* result, Exp* body);
-    /// Destroy a FunctionDec node.
-    virtual ~FunctionDec();
-    /** \} */
 
-    /// \name Visitors entry point.
-    /// \{ */
-    /// Accept a const visitor \a v.
-    void accept(ConstVisitor& v) const override;
-    /// Accept a non-const visitor \a v.
-    void accept(Visitor& v) override;
-    /// \}
+    /**
+     * \class ast::FunctionDec
+     * \brief FIXME
+     *
+     * FIXME
+     */
 
-    /** \name Accessors.
-     ** \{ */
-    /// Return formal arguments.
-    const VarDecs& formals_get() const;
-    /// Return formal arguments.
-    VarDecs& formals_get();
-    /// Return result type.
-    const NameTy* result_get() const;
-    /// Return result type.
-    NameTy* result_get();
-    /// Return instructions.
-    const Exp* body_get() const;
-    /// Return instructions.
-    Exp* body_get();
-    /// Set instructions.
-    void body_set(Exp*);
-    /** \} */
+    /// FunctionDec.
+    class FunctionDec : public Dec
+    {
+        public:
+            /** \name Ctor & dtor.
+             ** \{ */
+            /// Construct a FunctionDec node.
+            FunctionDec(const Location& location, const misc::symbol& name,
+                    VarDecs* formals, NameTy* result, Exp* body);
+            /// Destroy a FunctionDec node.
+            virtual ~FunctionDec();
+            /** \} */
 
-  protected:
-    /// Formal arguments.
-    VarDecs* formals_;
-    /// Result type.
-    NameTy* result_;
-    /// Instructions.
-    Exp* body_;
-  };
+            /**
+             * \name Visitors entry point.
+             * \{ */
+            /// Accept a const visitor \a v.
+            void accept(ConstVisitor& v) const override;
+            /// Accept a non-const visitor \a v.
+            void accept(Visitor& v) override;
+            /** \} */
+
+            /**
+             * \name Accessors.
+             * \{ */
+            /// Return formal arguments.
+            const VarDecs& formals_get() const;
+            /// Return formal arguments.
+            VarDecs& formals_get();
+            /// Return result type.
+            const NameTy* result_get() const;
+            /// Return result type.
+            NameTy* result_get();
+            /// Return instructions.
+            const Exp* body_get() const;
+            /// Return instructions.
+            Exp* body_get();
+            /// Set instructions.
+            void body_set(Exp*);
+            /** \} */
+
+        protected:
+            /// The formal arguments.
+            VarDecs* formals_;
+            /// The result type.
+            NameTy* result_;
+            /// The instructions.
+            Exp* body_;
+    };
 
 } // namespace ast
 
