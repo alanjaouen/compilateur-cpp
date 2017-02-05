c01ea6f7d7b5e6e34bdd98abe1d8927c240f15a5
Raphaël Caruso <altiire@gmail.com>
2017-02-05 12:02:59 +0100

    [TC2][AST][IF-EXP] commented header

--- src/ast/if-exp.hh
+++ src/ast/if-exp.hh
@@ -11,46 +11,59 @@
 namespace ast
 {
 
-  /// IfExp.
-  class IfExp : public Exp
-  {
-        /** \name Ctor & dtor.
-     ** \{ */
-    /// Construct a IfExp node.
-    IfExp(Location& location, Exp* test, Exp* Bthen, Exp* Belse = nullptr);
-    /// Destroy a IfExp node.
-    virtual ~IfExp();
-    /** \} */
 
-    /// \name Visitors entry point.
-    /// \{ */
-    /// Accept a const visitor \a v.
-    void accept(ConstVisitor& v) const override;
-    /// Accept a non-const visitor \a v.
-    void accept(Visitor& v) override;
-    /// \}
+    /**
+     * \class ast::IfExp.
+     * \brief FIXME
+     *
+     * FIXME
+     */
 
-    /** \name Accessors.
-     ** \{ */
-    /// Return test declaration.
-    const Exp& test_get() const;
-    /// Return test declaration.
-    Exp& test_get();
-    /// Return instructions executed in the then.
-    const Exp& then_get() const;
-    /// Return instructions executed in the then..
-    Exp& then_get();
-    /// Return instructions executed in the else clause.
-    const Exp& else_get() const;
-    /// Return instructions executed in the else clause.
-    Exp& else_get();
-    /** \} */
+    /// IfExp.
+    class IfExp : public Exp
+    {
+        public:
+            /** \name Ctor & dtor.
+             ** \{ */
+            /// Construct a IfExp node.
+            IfExp(const Location& location, Exp* test, Exp* Bthen,
+                  Exp* Belse = nullptr);
+            /// Destroy a IfExp node.
+            virtual ~IfExp();
+            /** \} */
 
-  protected:
-    Exp* test_;
-    Exp* Bthen_;
-    Exp* Belse_;
-  };
+            /// \name Visitors entry point.
+            /// \{ */
+            /// Accept a const visitor \a v.
+            void accept(ConstVisitor& v) const override;
+            /// Accept a non-const visitor \a v.
+            void accept(Visitor& v) override;
+            /// \}
+
+            /** \name Accessors.
+             ** \{ */
+            /// Return test declaration.
+            const Exp& test_get() const;
+            /// Return test declaration.
+            Exp& test_get();
+            /// Return instructions executed in the then.
+            const Exp& then_get() const;
+            /// Return instructions executed in the then..
+            Exp& then_get();
+            /// Return instructions executed in the else clause.
+            const Exp& else_get() const;
+            /// Return instructions executed in the else clause.
+            Exp& else_get();
+            /** \} */
+
+        protected:
+            /// The test declaration.
+            Exp* test_;
+            /// The then instruction.
+            Exp* Bthen_;
+            /// The else instruction.
+            Exp* Belse_;
+    };
 
 } // namespace ast
 
