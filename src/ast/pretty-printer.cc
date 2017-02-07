b43c002df401a1514e897d12802f629ed9839fe7
Raphaël Caruso <altiire@gmail.com>
2017-02-07 11:07:26 +0100

    [TC2][AST][PRETTY-PRINTER] implemented IfExp, LetExp, FunctionDec, Exp, ForExp, VarDec and NameTy printing

--- src/ast/pretty-printer.cc
+++ src/ast/pretty-printer.cc
@@ -15,62 +15,118 @@
 namespace ast
 {
 
-  // Anonymous namespace: these functions are private to this file.
-  namespace
-  {
-
-    /// \brief Output \a e on \a ostr.
-    ///
-    /// Used to factor the output of the name declared,
-    /// and its possible additional attributes.
-    inline
-    std::ostream&
-    operator<< (std::ostream& ostr, const Dec& e)
+    // Anonymous namespace: these functions are private to this file.
+    namespace
     {
-      ostr << e.name_get();
-      return ostr;
+
+        /// \brief Output \a e on \a ostr.
+        ///
+        /// Used to factor the output of the name declared,
+        /// and its possible additional attributes.
+        inline
+            std::ostream&
+            operator<< (std::ostream& ostr, const Dec& e)
+            {
+                ostr << e.name_get();
+                return ostr;
+            }
     }
-  }
 
 
 
-  PrettyPrinter::PrettyPrinter(std::ostream& ostr)
-    : ostr_(ostr)
-  {}
+    PrettyPrinter::PrettyPrinter(std::ostream& ostr)
+        : ostr_(ostr)
+    {}
 
 
-  void
-  PrettyPrinter::operator()(const SimpleVar& e)
-  {
-    ostr_ << e.name_get();
-  }
+    void
+        PrettyPrinter::operator()(const SimpleVar& e)
+        {
+            ostr_ << e.name_get();
+        }
 
-  void
-  PrettyPrinter::operator()(const FieldVar& e)
-  {
-  // FIXME: Some code was deleted here.
-  }
+    void
+        PrettyPrinter::operator()(const FieldVar& e)
+        {
+            // FIXME: Some code was deleted here.
+        }
 
-  /* Foo[10]. */
-  void
-  PrettyPrinter::operator()(const SubscriptVar& e)
-  {
-    ostr_ << e.var_get() << '[' << misc::incindent << e.index_get()
-          << misc::decindent << ']';
-  }
+    /* Foo[10]. */
+    void
+        PrettyPrinter::operator()(const SubscriptVar& e)
+        {
+            ostr_ << e.var_get() << '[' << misc::incindent << e.index_get()
+                << misc::decindent << ']';
+        }
 
-  void
-  PrettyPrinter::operator()(const CastVar& e)
-  {
-    ostr_ << "_cast(" << e.var_get() << ", " << e.ty_get() << ')';
-  }
+    void
+        PrettyPrinter::operator()(const CastVar& e)
+        {
+            ostr_ << "_cast(" << e.var_get() << ", " << e.ty_get() << ')';
+        }
 
-  void
-  PrettyPrinter::operator()(const CastExp& e)
-  {
-    ostr_ << "_cast(" << e.exp_get() << ", " << e.ty_get() << ')';
-  }
+    void
+        PrettyPrinter::operator()(const CastExp& e)
+        {
+            ostr_ << "_cast(" << e.exp_get() << ", " << e.ty_get() << ')';
+        }
 
-  // FIXME: Some code was deleted here.
+    void
+        PrettyPrinter::operator()(const IfExp& e)
+        {
+            ostr_ << "(if " << e.test_get() << misc::incindent
+                << "then " << e.then_get();
+            if (e.else_get() != nullptr)
+                ostr_ << misc::incindent
+                    << "else " << e.else_get() << ')' << misc::decindent;
+            else
+                ostr_ << ')' << misc::decindent;
+        }
+
+    void
+        PrettyPrinter::operator()(const LetExp& e)
+        {
+            ostr_ << "let" << misc::incindent
+                << e.decs_get() << misc::decindent
+                << "in" << misc::incindent
+                << e.seq_get() << misc::decindent
+                << "end;" << misc::decindent;
+        }
+
+    void
+        PrettyPrinter::operator()(const FunctionDec& e)
+        {
+            ostr_ << "function " << e.formals_get() << ':' << e.result_get()
+                    << '=' << misc::incindent
+                << '(' << e.body_get() << ')' << misc::decindent;
+        }
+
+    void
+        PrettyPrinter::operator()(const Exp& e)
+        {
+            ostr_ << '(' << e << ')' << misc::decindent;
+        }
+
+    void
+        PrettyPrinter::operator()(const ForExp& e)
+        {
+            ostr_ << "(for " << e.vardec_get() << " to " << e.hi_get() << " do"
+                    << misc::incindent
+                << e.body_get() << ");" << misc::decindent;
+        }
+
+    void
+        PrettyPrinter::operator()(const VarDec& e)
+        {
+            ostr_ << e.type_name_get() << " : " << e.init_get();
+        }
+
+    void
+        PrettyPrinter::operator()(const NameTy& e)
+        {
+            ostr_ << e.name_get();
+        }
+
+    // FIXME: Some code was deleted here.
 
 } // namespace ast
--- src/ast/pretty-printer.hh
+++ src/ast/pretty-printer.hh
@@ -11,39 +11,46 @@
 namespace ast
 {
 
-  /// Visit an Ast and print the content of each node.
-  class PrettyPrinter
-    : virtual public DefaultConstVisitor
-    , virtual public ObjectConstVisitor
-  {
-  public:
-    using super_type = DefaultConstVisitor;
-    // Import overloaded virtual functions.
-    using super_type::operator();
+    /// Visit an Ast and print the content of each node.
+    class PrettyPrinter
+        : virtual public DefaultConstVisitor
+          , virtual public ObjectConstVisitor
+    {
+        public:
+            using super_type = DefaultConstVisitor;
+            // Import overloaded virtual functions.
+            using super_type::operator();
 
-    /// Build to print on \a ostr.
-    PrettyPrinter(std::ostream& ostr);
+            /// Build to print on \a ostr.
+            PrettyPrinter(std::ostream& ostr);
 
-    /// Visit methods.
-    /// \{
-    void operator()(const SimpleVar& e) override;
-    void operator()(const FieldVar& e) override;
-    void operator()(const SubscriptVar& e) override;
-    void operator()(const CastVar& e) override;
-    void operator()(const CastExp& e) override;
-  // FIXME: Some code was deleted here.
-    /// \}
+            /// Visit methods.
+            /// \{
+            void operator()(const SimpleVar& e) override;
+            void operator()(const FieldVar& e) override;
+            void operator()(const SubscriptVar& e) override;
+            void operator()(const CastVar& e) override;
+            void operator()(const CastExp& e) override;
+            void operator()(const IfExp& e) override;
+            void operator()(const LetExp& e) override;
+            void operator()(const FunctionDec& e) override;
+            void operator()(const Exp& e);
+            void operator()(const ForExp& e) override;
+            void operator()(const VarDec& e) override;
+            void operator()(const NameTy& e) override;
+            // FIXME: Some code was deleted here.
+            /// \}
 
-  private:
-    // Factor pretty-printing of RecordExp and RecordTy.
-    template <typename RecordClass>
-    void print_record(const RecordClass& e);
+        private:
+            // Factor pretty-printing of RecordExp and RecordTy.
+            template <typename RecordClass>
+                void print_record(const RecordClass& e);
 
 
-  protected:
-    /// The stream to print on.
-    std::ostream& ostr_;
-  };
+        protected:
+            /// The stream to print on.
+            std::ostream& ostr_;
+    };
 
 
 } // namespace ast
