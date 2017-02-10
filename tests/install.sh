e083ed8c564cd723148c1ed13c167669bcccd17a
Raphaël Caruso <altiire@gmail.com>
2017-02-10 18:19:13 +0100

    [TC2][INSTALL] added 2 pypi projet for the fuzzing and the interactive mode

--- tests/install.sh
+++ tests/install.sh
@@ -5,3 +5,5 @@ echo "Installing dependencies for the test suite"
 pip install --user pyyaml
 pip install --user click
 pip install --user termcolor
+pip install --user pythondialog
+pip install --user rstr
