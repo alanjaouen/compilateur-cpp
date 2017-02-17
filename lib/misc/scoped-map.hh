e0014a3a6679122fda19e202e0764152501c2d8a
Clyde Caradine <clydejean.caradine@epita.fr>
2017-02-17 22:31:47 +0100

    [TC3][SCOPED-MAP] fix all methods of scoped-map.h* still need to test it

--- lib/misc/scoped-map.hh
+++ lib/misc/scoped-map.hh
@@ -21,33 +21,52 @@ namespace misc
   template <typename Key, typename Data>
   class scoped_map
   {
+    //Fixed
   public:
-    using scope_slot = std::map<Key&, Data&>;
-    using scope_type = std::vector<scope_slot&>;
+    using scope_slot = std::map<Key, Data>;
+    using scope_type = std::vector<scope_slot>;
 
     scoped_map();
-    scoped_map(const Key& key, const Data& value);
+    // scoped_map(const Key key, const Data value);
     // needed by lrde tests
-    scoped_map();
-    ~scoped_map();
+    ~scoped_map() = default;
 
+    ///Associate value to key in the current scope.
     void put (const Key& key, const Data& value);
+    ///If key was associated to some Data in the open scopes, return the most recent insertion. Otherwise, if Data is a pointer type, then return the empty pointer, else throw a std::range_error. To implement this feature, see <type_traits>
     Data get (const Key& key) const;
+    ///Send the content of this table on ostr in a human-readable manner, and return the stream. 
     std::ostream& dump (std::ostream& ostr) const;
+    ///Open a new scope.
     void scope_begin ();
+    ///Close the last scope, forgetting everything since the latest scope_begin(). 
     void scope_end ();
 
-    const scope_type& scopes_get() const;
+    const scope_type scopes_get() const;
   private:
 
-    scope_type& scopes_;
+    scope_type scopes_;
+    //End Fixed
   };
 
   template <typename Key, typename Data>
   std::ostream&
   operator<<(std::ostream& ostr, const scoped_map<Key, Data>& tbl);
 
-  // FIXME: Some code was deleted here.
+  //Error function.
+  template <typename Key, typename Data>
+  std::enable_if_t<std::is_pointer<Data>::value, Data>
+  throw_range_error()
+  {
+    return nullptr;
+  }
+
+  template <typename Key, typename Data>
+  std::enable_if_t<!std::is_pointer<Data>::value, Data>
+  throw_range_error()
+  {
+    throw(std::range_error("Impossible to find element in the current scope."));
+  }
 
 } // namespace misc
 
--- lib/misc/scoped-map.hxx
+++ lib/misc/scoped-map.hxx
@@ -17,41 +17,68 @@ namespace misc
 
   template <typename Key, typename Data>
   scoped_map<Key, Data>::scoped_map() :
-    scopes_(new scope_type())
+    scopes_()
   {}
 
-  template <typename Key, typename Data>
-  scoped_map<Key, Data>::scoped_map(const Key& key, const Data& value)
-  {
-    scope_type* list = new scope_type();
-    scope_slot& slot = new scope_slot(key, value);
-    list.push_back(slot);
-    scopes_ = list;
-  }
+  // template <typename Key, typename Data>
+  // scoped_map<Key, Data>::scoped_map(const Key key, const Data value)
+  // {
+  //   scope_type* list = new scope_type();
+  //   scope_slot& slot = new scope_slot(key, value);
+  //   list.push_back(slot);
+  //   scopes_ = list;
+  // }
 
   template <typename Key, typename Data>
   void scoped_map<Key, Data>::put(const Key& key, const Data& value)
-  {}
+  {
+    scopes_.back()[key] = value;
+  }
 
   template <typename Key, typename Data>
   Data scoped_map<Key, Data>::get(const Key& key) const
-  {}
+  {
+    for (auto i = scopes_.rbegin(); i != scopes_.rend(); i++)
+      {
+	auto res = i->find(key);
+	if (res != i->end())
+	  return res->second;
+      }
+    return throw_range_error<Key, Data>();
+    //return res->second;
+  }
 
   template <typename Key, typename Data>
   std::ostream& scoped_map<Key, Data>::dump(std::ostream& ostr) const
-  {}
+  {
+    for (const auto& i : scopes_)
+      {
+        ostr << "( ";
+        for (const auto& j : i)
+          ostr << j.first << ", ";
+        ostr << " )";
+      }
+    return ostr;
+  }
 
   template <typename Key, typename Data>
   void scoped_map<Key, Data>::scope_begin()
-  {}
+  {
+    scopes_.push_back(scope_slot());
+  }
 
   template <typename Key, typename Data>
   void scoped_map<Key, Data>::scope_end()
-  {}
+  {
+    scopes_.pop_back();
+  }
 
   template <typename Key, typename Data>
-  const typename scoped_map<Key, Data>::scope_type& scoped_map<Key, Data>::scopes_get() const
-  {}
+  inline const typename scoped_map<Key, Data>::scope_type
+  scoped_map<Key, Data>::scopes_get() const
+  {
+    return scopes_;
+  }
 
   template <typename Key, typename Data>
   inline std::ostream&
