800bc1ad6e51d26a14cf6a8195b4f04d6933876f
François Forestier <forest_b@epita.fr>
2017-02-19 04:40:24 +0100

    [TC3][TASK] start writing binding class

--- src/bind/tasks.cc
+++ src/bind/tasks.cc
@@ -3,4 +3,35 @@
  ** \brief Bind module tasks implementation.
  */
 
-  // FIXME: Some code was deleted here.
+  // FIXED forest_b
+
+#include <bind/libbind.hh>
+#define DEFINE_TASKS 4
+#include <bind/tasks.hh>
+#undef DEFINE_TASKS
+
+namespace bind
+{
+  namespace tasks
+  {
+    void bound()
+    {
+      
+    }
+
+    void bind()
+    {
+      
+    }
+
+    void bind_display()
+    {
+      
+    }
+
+    void object_bind()
+    {
+      
+    }
+  }
+}
