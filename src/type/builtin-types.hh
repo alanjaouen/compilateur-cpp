ad8643e4d4e42366ebc4a802c0fb3213482f9117
François Forestier <forest_b@epita.fr>
2017-03-17 23:51:59 +0100

    [TC4][TYPE] start builtin type

--- src/type/builtin-types.hh
+++ src/type/builtin-types.hh
@@ -4,12 +4,37 @@
  */
 #pragma once
 
-#include <type/type.hh>
 #include <type/fwd.hh>
+#include <type/type.hh>
 
 namespace type
 {
+// FIXME: Some code was deleted here (Other types : String, Int, Void).
 
-  // FIXME: Some code was deleted here (Other types : String, Int, Void).
+template <class D>
+class Singleton
+{
+  Singleton(const Singleton&) = delete;
+  Singleton(Singleton&&) = delete;
+  Singleton& operator=(const Singleton&) = delete;
+  Singleton& operator=(Singleton&&) = delete;
+  static const Singleton& instance()
+  {
+    static Singleton instance;
+    return instance;
+  }
+};
+
+class String : public Singleton<String>, public Type
+{
+};
+
+class Int : public Singleton<Int>, public Type
+{
+};
+
+class Void : public Singleton<Void>, public Type
+{
+};
 
 } // namespace type
